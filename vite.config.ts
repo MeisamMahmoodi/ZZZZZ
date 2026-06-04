983ccc3c7df59c70d2920367cd23c47c1193239d
MeisamMahmoodi <160858096+MeisamMahmoodi@users.noreply.github.com>
2026-06-04 17:41:49 +0200

    Fix Workbox cache size

--- vite.config.ts
+++ vite.config.ts
@@ -34,6 +34,7 @@ export default defineConfig({
         ],
       },
       workbox: {
+        maximumFileSizeToCacheInBytes: 3 * 1024 * 1024,
         // Cache app shell + static assets
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         // Inject the push event handler into the generated SW
